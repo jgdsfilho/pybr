dce853e81db9e41a870a75a846f32d9148e63863
Genilson Filho <jose.genilson@evolux.net.br>
2019-10-23 16:16:42 -0300

    Make sample to models

--- models.py
+++ models.py
@@ -5,3 +5,15 @@ Base = declarative_base
 
 class Task(Base):
     # and so on, because literally everything's the same...
+
+
+##### como criar modelo (igual slqalchemy normal)
+ from sqlalchemy import Column, BigInteger, String
+ from tornado_sqlalchemy import declarative_base
+
+ DeclarativeBase = declarative_base()
+
+ class User(DeclarativeBase):
+     id = Column(BigInteger, primary_key=True)
+     username = Column(String(255), unique=True)
+
