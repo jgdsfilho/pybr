9fe32d62e694f77898264680a87a8cf64b4f9e23
Genilson Filho <jose.genilson@evolux.net.br>
2019-10-23 17:53:59 -0300

    Creta model Roles

--- __init__.py
+++ __init__.py
@@ -3,21 +3,24 @@ from tornado.ioloop import IOLoop
 from tornado.options import define, options
 from tornado.web import Application
 
-from pybr.views import HelloWorld
+from views import HelloWorld
 
 import os
 from tornado_sqlalchemy import make_session_factory
 
 define('port', default=8888, help='port to list on')
-factory = make_session_factory(os.environ.get('DATABASE_URL', ''))
+#factory = make_session_factory(os.environ.get('DATABASE_URL', ''))
 def main():
 
     app = Application([
         ('/', HelloWorld)
     ],
-        session_factory=factory
+ #       session_factory=factory
     )
-    hasattrtp_server = HTTPServer(app)
+    http_server = HTTPServer(app)
     http_server.listen(options.port)
     print('Listening on http://localhost:%i' % options.port)
     IOLoop.current().start()
+
+if __name__== '__main__':
+    main()
