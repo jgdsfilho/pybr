3e7eb0a942a5b89393dd87da02128719ab5d3985
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-10-24 08:06:13 -0300

    Basic mongo config

--- __init__.py
+++ __init__.py
@@ -1,29 +1,52 @@
+import json
+
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.options import define, options
 from tornado.web import Application, RequestHandler
 
-from views import RolesHandler
+from motor import motor_tornado
 
-import os
-from tornado_sqlalchemy import make_session_factory
+from views import RolesHandler
 
 define('port', default=8888, help='port to list on')
 
 
-class MainHandler(RequestHandler):
+class BaseView(RequestHandler):
+    """Base view for this application."""
+
+    def prepare(self):
+        self.form_data = {
+            key: [val.decode('utf8') for val in val_list]
+            for key, val_list in self.request.arguments.items()
+        }
+
+    def set_default_headers(self):
+        """Set the default response header to be JSON."""
+        self.set_header("Content-Type", 'application/json; charset="utf-8"')
+
+    def send_response(self, data, status=200):
+        """Construct and send a JSON response with appropriate status code."""
+        self.set_status(status)
+        self.write(json.dumps(data))
+
+
+class MainHandler(BaseView):
     def get(self):
-        """Handle a GET request for saying Hello World!."""
         self.write("Olá galera da Python Brasil 2019! "
                    "Para ver os rolês faça requisições para '/roles'")
 
 
 def main():
 
+    client = motor_tornado.MotorClient('localhost', 27017)
+    db = client.test_db
+
     app = Application([
         ('/', MainHandler),
         ('/roles', RolesHandler),
        ],
+       db=db
     )
 
     http_server = HTTPServer(app)
--- views.py
+++ views.py
@@ -2,4 +2,5 @@ from tornado.web import RequestHandler
 
 class RolesHandler(RequestHandler):
     def get(self):
+        db = self.settings['db']
         self.write("Hello, world!")
