fc5705ab65269e301b0e70fe7d06de127866b759
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-10-24 08:47:30 -0300

    API POST

--- __init__.py
+++ __init__.py
@@ -7,28 +7,9 @@ from tornado.web import Application, RequestHandler
 
 from motor import motor_tornado
 
-from views import RolesHandler
+from views import BaseView, RolesHandler
 
 define('port', default=8888, help='port to list on')
-
-
-class BaseView(RequestHandler):
-    """Base view for this application."""
-
-    def prepare(self):
-        self.form_data = {
-            key: [val.decode('utf8') for val in val_list]
-            for key, val_list in self.request.arguments.items()
-        }
-
-    def set_default_headers(self):
-        """Set the default response header to be JSON."""
-        self.set_header("Content-Type", 'application/json; charset="utf-8"')
-
-    def send_response(self, data, status=200):
-        """Construct and send a JSON response with appropriate status code."""
-        self.set_status(status)
-        self.write(json.dumps(data))
 
 
 class MainHandler(BaseView):
--- views.py
+++ views.py
@@ -1,6 +1,46 @@
+import json
+
 from tornado.web import RequestHandler
 
-class RolesHandler(RequestHandler):
+
+class BaseView(RequestHandler):
+    """Base view for this application."""
+
+    def set_default_headers(self):
+        """Set the default response header to be JSON."""
+        self.set_header("Content-Type", 'application/json; charset="utf-8"')
+
+    def send_response(self, data, status=200):
+        """Construct and send a JSON response with appropriate status code."""
+        self.set_status(status)
+        self.write(json.dumps(data))
+
+
+
+class RolesHandler(BaseView):
     def get(self):
-        db = self.settings['db']
         self.write("Hello, world!")
+
+    def post(self):
+        required_params = ('nome', 'endereco', 'data')
+        acceptable_params = (
+            'hora', 'preco_da_cerveja', 'tem_karaoke', 'quem_vai'
+        )
+        errors = []
+        body = json.loads(self.request.body)
+        for param in required_params:
+            if param not in body:
+                errors.append({param: 'Required parameter'})
+        for param in body:
+            if param not in required_params + acceptable_params:
+                errors.append({param: 'Unexpected parameter'})
+        if errors:
+            self.send_response({'errors': errors}, status=400)
+            return
+
+        self._do_insert_one(body)
+        self.send_response({'message': 'Role successfully inserted'})
+
+    def _do_insert_one(self, data, collection='role_collection'):
+        db = self.settings['db']
+        result = db[collection].insert_one(data)
